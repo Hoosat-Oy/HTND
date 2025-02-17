97e7d2fc04bf63d5ada89a6f96b3f438050c2cdf
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-02-17 16:14:24 +0200

    Try to do db.compact if the error is not found (testing)

--- domain/prefixmanager/prefix.go
+++ domain/prefixmanager/prefix.go
@@ -50,7 +50,7 @@ func InactivePrefix(dataAccessor database.DataAccessor) (*prefix.Prefix, bool, e
 func DeleteInactivePrefix(db database.Database) error {
 	prefixBytes, err := db.Get(inactivePrefixKey)
 	if database.IsNotFoundError(err) {
-		return nil
+		return db.Compact()
 	}
 
 	if err != nil {
