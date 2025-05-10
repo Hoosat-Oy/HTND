7470635900f7e688c7ed9e15b88928f8614eb8d3
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-05-10 12:27:39 +0300

    Lets try to thread handle block relay request

--- app/protocol/flows/v5/blockrelay/handle_relay_block_requests.go
+++ app/protocol/flows/v5/blockrelay/handle_relay_block_requests.go
@@ -1,12 +1,14 @@
 package blockrelay
 
 import (
+	"sync"
 	"time"
 
 	"github.com/Hoosat-Oy/HTND/app/appmessage"
 	peerpkg "github.com/Hoosat-Oy/HTND/app/protocol/peer"
 	"github.com/Hoosat-Oy/HTND/app/protocol/protocolerrors"
 	"github.com/Hoosat-Oy/HTND/domain"
+	"github.com/Hoosat-Oy/HTND/domain/consensus/model/externalapi"
 	"github.com/Hoosat-Oy/HTND/domain/consensus/utils/constants"
 	"github.com/Hoosat-Oy/HTND/domain/consensus/utils/pow"
 	"github.com/Hoosat-Oy/HTND/infrastructure/network/netadapter/router"
@@ -29,48 +31,67 @@ func HandleRelayBlockRequests(context RelayBlockRequestsContext, incomingRoute *
 			return err
 		}
 		getRelayBlocksMessage := message.(*appmessage.MsgRequestRelayBlocks)
-		// log.Debugf("Got request for relay blocks with hashes %s", getRelayBlocksMessage.Hashes)
-		for _, hash := range getRelayBlocksMessage.Hashes {
-			// Fetch the block from the database.
 
-			block, found, err := context.Domain().Consensus().GetBlock(hash)
-			if err != nil {
-				return errors.Wrapf(err, "unable to fetch requested block hash %s", hash)
-			}
-			if !found {
-				return protocolerrors.Errorf(false, "Relay block %s not found", hash)
-			}
+		var wg sync.WaitGroup
+		errChan := make(chan error, len(getRelayBlocksMessage.Hashes))
 
-			if block.PoWHash == "" && block.Header.Version() >= constants.PoWIntegrityMinVersion {
-				powHashFound := false
-				for i := 0; i < 5; i++ {
-					time.Sleep(30 * time.Millisecond)
-					block, found, err = context.Domain().Consensus().GetBlock(hash)
-					if err != nil {
-						return errors.Wrapf(err, "unable to fetch requested block hash %s", hash)
+		for i := 0; i < len(getRelayBlocksMessage.Hashes); i++ {
+			hash := getRelayBlocksMessage.Hashes[i]
+
+			wg.Add(1)
+			go func(hash *externalapi.DomainHash) {
+				defer wg.Done()
+
+				block, found, err := context.Domain().Consensus().GetBlock(hash)
+				if err != nil {
+					errChan <- errors.Wrapf(err, "unable to fetch requested block hash %s", hash)
+					return
+				}
+				if !found {
+					errChan <- protocolerrors.Errorf(false, "Relay block %s not found", hash)
+					return
+				}
+
+				if block.PoWHash == "" && block.Header.Version() >= constants.PoWIntegrityMinVersion {
+					powHashFound := false
+					for j := 0; j < 5; j++ {
+						time.Sleep(30 * time.Millisecond)
+						block, found, err = context.Domain().Consensus().GetBlock(hash)
+						if err != nil {
+							errChan <- errors.Wrapf(err, "unable to re-fetch requested block hash %s", hash)
+							return
+						}
+						if !found {
+							errChan <- protocolerrors.Errorf(false, "Relay block %s not found on retry", hash)
+							return
+						}
+						if block.PoWHash != "" {
+							powHashFound = true
+							break
+						}
 					}
-					if !found {
-						return protocolerrors.Errorf(false, "Relay block %s not found", hash)
-					}
-					if block.PoWHash != "" {
-						// log.Debugf("Found PoW Hash for relayed block: Hash %s, Pow hash %s", hash, block.PoWHash)
-						powHashFound = true
-						break
+					if !powHashFound {
+						state := pow.NewState(block.Header.ToMutable())
+						_, powHash := state.CalculateProofOfWorkValue()
+						block.PoWHash = powHash.String()
 					}
 				}
-				if !powHashFound {
-					state := pow.NewState(block.Header.ToMutable())
-					_, powHash := state.CalculateProofOfWorkValue()
-					block.PoWHash = powHash.String()
-					// log.Debugf("Recalculated PoW Hash for relayed block: Hash %s, Pow hash %s", hash, block.PoWHash)
-				}
-			}
 
-			err = outgoingRoute.Enqueue(appmessage.DomainBlockToMsgBlock(block))
+				err = outgoingRoute.Enqueue(appmessage.DomainBlockToMsgBlock(block))
+				if err != nil {
+					errChan <- errors.Wrapf(err, "failed to enqueue block %s", hash)
+					return
+				}
+			}(hash)
+		}
+
+		wg.Wait()
+		close(errChan)
+
+		for err := range errChan {
 			if err != nil {
 				return err
 			}
-			// log.Debugf("Relayed block: Hash %s, Pow hash %s, Version %d >= %d, Peer %s", hash, block.PoWHash, block.Header.Version(), constants.PoWIntegrityMinVersion, peer.Connection().Address())
 		}
 	}
 }
