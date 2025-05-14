8a37905205724ceb5eb3870fdcaea5ffd98dc37f
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-05-14 14:49:18 +0300

    Lets thread the block fetching, but not sending. Testing this.

--- app/protocol/flows/v5/blockrelay/handle_relay_block_requests.go
+++ app/protocol/flows/v5/blockrelay/handle_relay_block_requests.go
@@ -1,12 +1,14 @@
 package blockrelay
 
 import (
+	"sync"
 	"time"
 
 	"github.com/Hoosat-Oy/HTND/app/appmessage"
 	peerpkg "github.com/Hoosat-Oy/HTND/app/protocol/peer"
 	"github.com/Hoosat-Oy/HTND/app/protocol/protocolerrors"
 	"github.com/Hoosat-Oy/HTND/domain"
+	"github.com/Hoosat-Oy/HTND/domain/consensus/model/externalapi"
 	"github.com/Hoosat-Oy/HTND/domain/consensus/utils/constants"
 	"github.com/Hoosat-Oy/HTND/domain/consensus/utils/pow"
 	"github.com/Hoosat-Oy/HTND/infrastructure/network/netadapter/router"
@@ -18,125 +20,131 @@ type RelayBlockRequestsContext interface {
 	Domain() domain.Domain
 }
 
+type result struct {
+	index int
+	block *externalapi.DomainBlock
+	err   error
+}
+
 // HandleRelayBlockRequests listens to appmessage.MsgRequestRelayBlocks messages and sends
 // their corresponding blocks to the requesting peer.
 func HandleRelayBlockRequests(context RelayBlockRequestsContext, incomingRoute *router.Route,
 	outgoingRoute *router.Route, peer *peerpkg.Peer) error {
-	for {
-		message, err := incomingRoute.Dequeue()
-		if err != nil {
-			return err
-		}
-		getRelayBlocksMessage := message.(*appmessage.MsgRequestRelayBlocks)
-		for _, hash := range getRelayBlocksMessage.Hashes {
-			// Fetch the block from the database.
-
-			block, found, err := context.Domain().Consensus().GetBlock(hash)
-			if err != nil {
-				return errors.Wrapf(err, "unable to fetch requested block hash %s", hash)
-			}
-			if !found {
-				return protocolerrors.Errorf(false, "Relay block %s not found", hash)
-			}
-
-			if block.PoWHash == "" && block.Header.Version() >= constants.PoWIntegrityMinVersion {
-				powHashFound := false
-				for i := 0; i < 5; i++ {
-					time.Sleep(30 * time.Millisecond)
-					block, found, err = context.Domain().Consensus().GetBlock(hash)
-					if err != nil {
-						return errors.Wrapf(err, "unable to fetch requested block hash %s", hash)
-					}
-					if !found {
-						return protocolerrors.Errorf(false, "Relay block %s not found", hash)
-					}
-					if block.PoWHash != "" {
-						// log.Debugf("Found PoW Hash for relayed block: Hash %s, Pow hash %s", hash, block.PoWHash)
-						powHashFound = true
-						break
-					}
-				}
-				if !powHashFound {
-					state := pow.NewState(block.Header.ToMutable())
-					_, powHash := state.CalculateProofOfWorkValue()
-					block.PoWHash = powHash.String()
-					// log.Debugf("Recalculated PoW Hash for relayed block: Hash %s, Pow hash %s", hash, block.PoWHash)
-				}
-			}
-
-			err = outgoingRoute.Enqueue(appmessage.DomainBlockToMsgBlock(block))
-			if err != nil {
-				return err
-			}
-		}
-	}
 	// for {
 	// 	message, err := incomingRoute.Dequeue()
 	// 	if err != nil {
 	// 		return err
 	// 	}
 	// 	getRelayBlocksMessage := message.(*appmessage.MsgRequestRelayBlocks)
+	// 	for _, hash := range getRelayBlocksMessage.Hashes {
+	// 		// Fetch the block from the database.
 
-	// 	var wg sync.WaitGroup
-	// 	errChan := make(chan error, len(getRelayBlocksMessage.Hashes))
+	// 		block, found, err := context.Domain().Consensus().GetBlock(hash)
+	// 		if err != nil {
+	// 			return errors.Wrapf(err, "unable to fetch requested block hash %s", hash)
+	// 		}
+	// 		if !found {
+	// 			return protocolerrors.Errorf(false, "Relay block %s not found", hash)
+	// 		}
 
-	// 	for i := 0; i < len(getRelayBlocksMessage.Hashes); i++ {
-	// 		hash := getRelayBlocksMessage.Hashes[i]
-
-	// 		wg.Add(1)
-	// 		go func(hash *externalapi.DomainHash) {
-	// 			defer wg.Done()
-
-	// 			block, found, err := context.Domain().Consensus().GetBlock(hash)
-	// 			if err != nil {
-	// 				errChan <- errors.Wrapf(err, "unable to fetch requested block hash %s", hash)
-	// 				return
-	// 			}
-	// 			if !found {
-	// 				errChan <- protocolerrors.Errorf(false, "Relay block %s not found", hash)
-	// 				return
-	// 			}
-
-	// 			if block.PoWHash == "" && block.Header.Version() >= constants.PoWIntegrityMinVersion {
-	// 				powHashFound := false
-	// 				for j := 0; j < 5; j++ {
-	// 					time.Sleep(30 * time.Millisecond)
-	// 					block, found, err = context.Domain().Consensus().GetBlock(hash)
-	// 					if err != nil {
-	// 						errChan <- errors.Wrapf(err, "unable to re-fetch requested block hash %s", hash)
-	// 						return
-	// 					}
-	// 					if !found {
-	// 						errChan <- protocolerrors.Errorf(false, "Relay block %s not found on retry", hash)
-	// 						return
-	// 					}
-	// 					if block.PoWHash != "" {
-	// 						powHashFound = true
-	// 						break
-	// 					}
+	// 		if block.PoWHash == "" && block.Header.Version() >= constants.PoWIntegrityMinVersion {
+	// 			powHashFound := false
+	// 			for i := 0; i < 5; i++ {
+	// 				time.Sleep(30 * time.Millisecond)
+	// 				block, found, err = context.Domain().Consensus().GetBlock(hash)
+	// 				if err != nil {
+	// 					return errors.Wrapf(err, "unable to fetch requested block hash %s", hash)
 	// 				}
-	// 				if !powHashFound {
-	// 					state := pow.NewState(block.Header.ToMutable())
-	// 					_, powHash := state.CalculateProofOfWorkValue()
-	// 					block.PoWHash = powHash.String()
+	// 				if !found {
+	// 					return protocolerrors.Errorf(false, "Relay block %s not found", hash)
+	// 				}
+	// 				if block.PoWHash != "" {
+	// 					// log.Debugf("Found PoW Hash for relayed block: Hash %s, Pow hash %s", hash, block.PoWHash)
+	// 					powHashFound = true
+	// 					break
 	// 				}
 	// 			}
-
-	// 			err = outgoingRoute.Enqueue(appmessage.DomainBlockToMsgBlock(block))
-	// 			if err != nil {
-	// 				errChan <- errors.Wrapf(err, "failed to enqueue block %s", hash)
-	// 				return
+	// 			if !powHashFound {
+	// 				state := pow.NewState(block.Header.ToMutable())
+	// 				_, powHash := state.CalculateProofOfWorkValue()
+	// 				block.PoWHash = powHash.String()
+	// 				// log.Debugf("Recalculated PoW Hash for relayed block: Hash %s, Pow hash %s", hash, block.PoWHash)
 	// 			}
-	// 		}(hash)
-	// 	}
+	// 		}
 
-	// 	wg.Wait()
-	// 	close(errChan)
-
-	// 	for err := range errChan {
+	// 		err = outgoingRoute.Enqueue(appmessage.DomainBlockToMsgBlock(block))
 	// 		if err != nil {
 	// 			return err
 	// 		}
 	// 	}
 	// }
+	for {
+		message, err := incomingRoute.Dequeue()
+		if err != nil {
+			return err
+		}
+		getRelayBlocksMessage := message.(*appmessage.MsgRequestRelayBlocks)
+
+		results := make([]*result, len(getRelayBlocksMessage.Hashes))
+		var wg sync.WaitGroup
+
+		for i := 0; i < len(getRelayBlocksMessage.Hashes); i++ {
+			i := i
+			hash := getRelayBlocksMessage.Hashes[i]
+
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+
+				block, found, err := context.Domain().Consensus().GetBlock(hash)
+				if err != nil {
+					results[i] = &result{i, nil, errors.Wrapf(err, "unable to fetch block %s", hash)}
+					return
+				}
+				if !found {
+					results[i] = &result{i, nil, protocolerrors.Errorf(false, "Block %s not found", hash)}
+					return
+				}
+
+				if block.PoWHash == "" && block.Header.Version() >= constants.PoWIntegrityMinVersion {
+					powHashFound := false
+					for j := 0; j < 5; j++ {
+						time.Sleep(30 * time.Millisecond)
+						block, found, err = context.Domain().Consensus().GetBlock(hash)
+						if err != nil {
+							results[i] = &result{i, nil, errors.Wrapf(err, "re-fetch failed %s", hash)}
+							return
+						}
+						if !found {
+							results[i] = &result{i, nil, protocolerrors.Errorf(false, "Block %s not found on retry", hash)}
+							return
+						}
+						if block.PoWHash != "" {
+							powHashFound = true
+							break
+						}
+					}
+					if !powHashFound {
+						state := pow.NewState(block.Header.ToMutable())
+						_, powHash := state.CalculateProofOfWorkValue()
+						block.PoWHash = powHash.String()
+					}
+				}
+
+				results[i] = &result{i, block, nil}
+			}()
+		}
+
+		wg.Wait()
+
+		for _, r := range results {
+			if r.err != nil {
+				return r.err
+			}
+			err := outgoingRoute.Enqueue(appmessage.DomainBlockToMsgBlock(r.block))
+			if err != nil {
+				return errors.Wrapf(err, "failed to enqueue block at index %d", r.index)
+			}
+		}
+	}
 }
