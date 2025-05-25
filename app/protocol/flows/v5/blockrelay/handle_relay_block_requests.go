e9c5d39dcd72fb3fe817120356a65c81303689b6
Tonto <tonto@winblows>
2025-05-25 14:40:36 +0300

    Add exponential backoff to refetching block for relay.

--- app/protocol/flows/v5/blockrelay/handle_relay_block_requests.go
+++ app/protocol/flows/v5/blockrelay/handle_relay_block_requests.go
@@ -17,58 +17,12 @@ type RelayBlockRequestsContext interface {
 	Domain() domain.Domain
 }
 
+const getBlockRetryInterval = 100 * time.Millisecond
+
 // HandleRelayBlockRequests listens to appmessage.MsgRequestRelayBlocks messages and sends
 // their corresponding blocks to the requesting peer.
 func HandleRelayBlockRequests(context RelayBlockRequestsContext, incomingRoute *router.Route,
 	outgoingRoute *router.Route, peer *peerpkg.Peer) error {
-	// for {
-	// 	message, err := incomingRoute.Dequeue()
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	getRelayBlocksMessage := message.(*appmessage.MsgRequestRelayBlocks)
-	// 	for _, hash := range getRelayBlocksMessage.Hashes {
-	// 		// Fetch the block from the database.
-
-	// 		block, found, err := context.Domain().Consensus().GetBlock(hash)
-	// 		if err != nil {
-	// 			return errors.Wrapf(err, "unable to fetch requested block hash %s", hash)
-	// 		}
-	// 		if !found {
-	// 			return protocolerrors.Errorf(false, "Relay block %s not found", hash)
-	// 		}
-
-	// 		if block.PoWHash == "" && block.Header.Version() >= constants.PoWIntegrityMinVersion {
-	// 			powHashFound := false
-	// 			for i := 0; i < 5; i++ {
-	// 				time.Sleep(30 * time.Millisecond)
-	// 				block, found, err = context.Domain().Consensus().GetBlock(hash)
-	// 				if err != nil {
-	// 					return errors.Wrapf(err, "unable to fetch requested block hash %s", hash)
-	// 				}
-	// 				if !found {
-	// 					return protocolerrors.Errorf(false, "Relay block %s not found", hash)
-	// 				}
-	// 				if block.PoWHash != "" {
-	// 					// log.Debugf("Found PoW Hash for relayed block: Hash %s, Pow hash %s", hash, block.PoWHash)
-	// 					powHashFound = true
-	// 					break
-	// 				}
-	// 			}
-	// 			if !powHashFound {
-	// 				state := pow.NewState(block.Header.ToMutable())
-	// 				_, powHash := state.CalculateProofOfWorkValue()
-	// 				block.PoWHash = powHash.String()
-	// 				// log.Debugf("Recalculated PoW Hash for relayed block: Hash %s, Pow hash %s", hash, block.PoWHash)
-	// 			}
-	// 		}
-
-	// 		err = outgoingRoute.Enqueue(appmessage.DomainBlockToMsgBlock(block))
-	// 		if err != nil {
-	// 			return err
-	// 		}
-	// 	}
-	// }
 	for {
 		message, err := incomingRoute.Dequeue()
 		if err != nil {
@@ -90,9 +44,7 @@ func HandleRelayBlockRequests(context RelayBlockRequestsContext, incomingRoute *
 				}
 
 				if block.PoWHash == "" && block.Header.Version() >= constants.PoWIntegrityMinVersion {
-					powHashFound := false
 					for j := 0; j < 5; j++ {
-						time.Sleep(30 * time.Millisecond)
 						block, found, err = context.Domain().Consensus().GetBlock(hash)
 						if err != nil {
 							log.Warnf("unable to re-fetch requested block hash %s: %s", hash, err)
@@ -103,11 +55,11 @@ func HandleRelayBlockRequests(context RelayBlockRequestsContext, incomingRoute *
 							return
 						}
 						if block.PoWHash != "" {
-							powHashFound = true
 							break
 						}
+						time.Sleep(getBlockRetryInterval * time.Duration(i+1))
 					}
-					if !powHashFound {
+					if block.PoWHash == "" {
 						state := pow.NewState(block.Header.ToMutable())
 						_, powHash := state.CalculateProofOfWorkValue()
 						block.PoWHash = powHash.String()
