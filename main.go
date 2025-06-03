9b0d305c26dd391e957d94b1ed61f87e1a382598
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-06-03 11:23:15 +0300

    Drop memory limit back to 8gb and disable profiling.

--- main.go
+++ main.go
@@ -5,8 +5,6 @@
 package main
 
 import (
-	"log"
-	"net/http"
 	_ "net/http/pprof"
 	"os"
 	"runtime"
@@ -18,13 +16,12 @@ import (
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU()) // Set the maximum number of CPUs that can be executing simultaneously
 	debug.SetGCPercent(200)              // Set GC to run less aggressively
-	debug.SetMemoryLimit(16_000_000_000) // Set memory limit to 16GB
-	runtime.SetBlockProfileRate(1)       // Set block profile rate to 1 to enable block profiling
-	runtime.SetMutexProfileFraction(1)   // Set mutex profile fraction to 1 to enable mutex profiling
-
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
+	debug.SetMemoryLimit(8_000_000_000)  // Set memory limit to 8GB
+	//runtime.SetBlockProfileRate(1)       // Set block profile rate to 1 to enable block profiling
+	//runtime.SetMutexProfileFraction(1)   // Set mutex profile fraction to 1 to enable mutex profiling
+	// go func() {
+	// 	log.Println(http.ListenAndServe("localhost:6060", nil))
+	// }()
 	if err := app.StartApp(); err != nil {
 		os.Exit(1)
 	}
