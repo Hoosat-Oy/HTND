19d47c5a590c48d378a185bc385cba2826b88e0c
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-07-05 19:45:52 +0300

    Change leveldb settings

--- infrastructure/db/database/ldb/options.go
+++ infrastructure/db/database/ldb/options.go
@@ -7,11 +7,11 @@ func Options() opt.Options {
 	return opt.Options{
 		Compression:            opt.SnappyCompression, // Use Snappy to reduce I/O
 		NoSync:                 true,                  // Keep for write throughput
-		WriteBuffer:            64 * opt.MiB,          // Smaller buffer for smaller SSTs
+		WriteBuffer:            32 * opt.MiB,          // Smaller buffer for smaller SSTs
 		BlockCacheCapacity:     512 * opt.MiB,         // Keep for read performance
-		OpenFilesCacheCapacity: 2048,                  // Reduce slightly to limit open files
+		OpenFilesCacheCapacity: 512,                   // Reduce slightly to limit open files
 		BlockRestartInterval:   8,                     // Keep for faster read scans
 		CompactionTableSize:    8 * opt.MiB,           // Smaller tables for faster compactions
-		CompactionTotalSize:    512 * opt.MiB,         // Smaller total size to trigger compactions sooner
+		CompactionTotalSize:    1024 * opt.MiB,        // Smaller total size to trigger compactions sooner
 	}
 }
