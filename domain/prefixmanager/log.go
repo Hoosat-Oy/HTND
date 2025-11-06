de6a93ef2ec429d12a83bf4b792fbfefa3016a55
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-11-06 17:58:33 +0200

    Remove unused spawn var and panics import in prefixmanager/log.go

--- domain/prefixmanager/log.go
+++ domain/prefixmanager/log.go
@@ -2,8 +2,6 @@ package prefixmanager
 
 import (
 	"github.com/Hoosat-Oy/HTND/infrastructure/logger"
-	"github.com/Hoosat-Oy/HTND/util/panics"
 )
 
 var log = logger.RegisterSubSystem("PRFX")
-var spawn = panics.GoroutineWrapperFunc(log)
