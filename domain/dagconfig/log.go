e885c30f3f784b9bad13ff9e2071099d5f554b7b
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-11-06 14:38:35 +0200

    Remove unused log var and logger import in dagconfig/log.go

--- domain/dagconfig/log.go
+++ domain/dagconfig/log.go
@@ -3,9 +3,3 @@
 // license that can be found in the LICENSE file.
 
 package dagconfig
-
-import (
-	"github.com/Hoosat-Oy/HTND/infrastructure/logger"
-)
-
-var log = logger.RegisterSubSystem("DAGC")
