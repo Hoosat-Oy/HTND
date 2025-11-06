f8a6675637d8218176bc03c5b447632865875bbc
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-11-06 14:20:30 +0200

    Fix deprecated os.Kill usage in signal.Notify

--- cmd/htnwallet/keys/get_password.go
+++ cmd/htnwallet/keys/get_password.go
@@ -20,7 +20,7 @@ func GetPassword(prompt string) string {
 	// Restore it in the event of an interrupt.
 	// CITATION: Konstantin Shaposhnikov - https://groups.google.com/forum/#!topic/golang-nuts/kTVAbtee9UA
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, os.Kill)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
 		_ = term.Restore(int(syscall.Stdin), initialTermState)
