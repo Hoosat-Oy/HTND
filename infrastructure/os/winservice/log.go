3993e8ff18651ddd9ebce55615ba53a281dfcf3e
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-11-06 18:01:09 +0200

    Remove unused spawn var and panics import in winservice/log.go

--- infrastructure/os/winservice/log.go
+++ infrastructure/os/winservice/log.go
@@ -6,8 +6,6 @@ package winservice
 
 import (
 	"github.com/Hoosat-Oy/HTND/infrastructure/logger"
-	"github.com/Hoosat-Oy/HTND/util/panics"
 )
 
 var log = logger.RegisterSubSystem("CNFG")
-var spawn = panics.GoroutineWrapperFunc(log)
