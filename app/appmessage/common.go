11d6892805ac05ba8a81f8d0b0cda54b165d6099
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-11-06 17:58:59 +0200

    Remove unused errNonCanonicalVarInt and errNoEncodingForType vars and errors import in common.go

--- app/appmessage/common.go
+++ app/appmessage/common.go
@@ -4,17 +4,5 @@
 
 package appmessage
 
-import (
-	"github.com/pkg/errors"
-)
-
 // MaxInvPerMsg is the maximum number of inventory vectors that can be in any type of hoosat inv message.
 const MaxInvPerMsg = 1 << 17
-
-// errNonCanonicalVarInt is the common format string used for non-canonically
-// encoded variable length integer errors.
-var errNonCanonicalVarInt = "non-canonical varint %x - discriminant %x must " +
-	"encode a value greater than %x"
-
-// errNoEncodingForType signifies that there's no encoding for the given type.
-var errNoEncodingForType = errors.New("there's no encoding for this type")
