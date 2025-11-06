d583d49ed5fd886e29c8f501fafc234479960a7d
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-11-06 22:06:59 +0200

    Change default config values

--- infrastructure/config/config.go
+++ infrastructure/config/config.go
@@ -129,9 +129,9 @@ type Flags struct {
 	ProtocolVersion                 uint32        `long:"protocol-version" hidden:"true" description:"Use non default p2p protocol version"`
 
 	// Compound transaction rate limiting flags
-	MaxCompoundTxPerMinute    uint64 `long:"max-compound-tx-per-minute" description:"Maximum compound transactions per address per minute" default:"5"`
-	CompoundTxRateLimitWindow uint64 `long:"compound-tx-ratelimit-window" description:"Rate limit window in minutes" default:"5"`
-	CompoundTxInputsThreshold uint64 `long:"compound-tx-inputs-threshold" description:"Minimum inputs to consider transaction as compound" default:"10"`
+	MaxCompoundTxPerMinute    uint64 `long:"max-compound-tx-per-minute" description:"Maximum compound transactions per address per minute" default:"10"`
+	CompoundTxRateLimitWindow uint64 `long:"compound-tx-ratelimit-window" description:"Rate limit window in minutes" default:"1"`
+	CompoundTxInputsThreshold uint64 `long:"compound-tx-inputs-threshold" description:"Minimum inputs to consider transaction as compound" default:"21"`
 
 	// Wallet freezing flags
 	FrozenAddresses []string `long:"freeze-address" description:"Address to freeze (can be specified multiple times)"`
