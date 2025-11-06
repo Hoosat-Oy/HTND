9efae642cf1c7b978c853cd1f4851f73307fd3df
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-11-06 14:10:29 +0200

    Update P2P server to use grpc.NewClient instead of deprecated DialContext
    Replace grpc.DialContext with grpc.NewClient
    Use grpc.WithTransportCredentials(insecure.NewCredentials()) instead of grpc.WithInsecure()
    Add grpc.WithTimeout for dial timeout handling
    Remove manual context creation as it's handled by grpc.WithTimeout

--- infrastructure/network/netadapter/server/grpcserver/p2pserver.go
+++ infrastructure/network/netadapter/server/grpcserver/p2pserver.go
@@ -10,6 +10,7 @@ import (
 	"github.com/Hoosat-Oy/HTND/util/panics"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/encoding/gzip"
 	"google.golang.org/grpc/peer"
 )
@@ -50,10 +51,7 @@ func (p *p2pServer) Connect(address string) (server.Connection, error) {
 	// connection failures (context deadline exceeded) before a TCP handshake completes.
 	// Bump this to a more forgiving value to improve initial connectivity.
 	const dialTimeout = 5 * time.Second
-	ctx, cancel := context.WithTimeout(context.Background(), dialTimeout)
-	defer cancel()
-
-	gRPCClientConnection, err := grpc.DialContext(ctx, address, grpc.WithInsecure(), grpc.WithBlock())
+	gRPCClientConnection, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock(), grpc.WithTimeout(dialTimeout))
 	if err != nil {
 		return nil, errors.Wrapf(err, "%s error connecting to %s", p.name, address)
 	}
