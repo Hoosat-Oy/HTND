55369ccccb84c79ca9a089f3dc9210480c819cb4
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-11-09 23:57:20 +0200

    if utxoentry missing for input, set transaction as not accepted.

--- domain/consensus/processes/consensusstatemanager/calculate_past_utxo.go
+++ domain/consensus/processes/consensusstatemanager/calculate_past_utxo.go
@@ -200,9 +200,7 @@ func (csm *consensusStateManager) applyMergeSetBlocks(stagingArea *model.Staging
 				transactionInputUTXOEntries = make([]externalapi.UTXOEntry, len(transaction.Inputs))
 				for k, input := range transaction.Inputs {
 					if input.UTXOEntry == nil {
-						log.Debugf("Transaction %s in block %s has nil UTXOEntry for input %d, skipping",
-							consensushashing.TransactionID(transaction), mergeSetBlockHash, k)
-						continue
+						isAccepted = false
 					}
 					transactionInputUTXOEntries[k] = input.UTXOEntry
 				}
