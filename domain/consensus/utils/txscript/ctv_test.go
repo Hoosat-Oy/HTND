cbf67aa3d68e23fb2362df2c2164b29d22cd6f47
Tonto <tonto@winblows>
2026-01-16 13:14:18 +0200

    More CTV tests

--- domain/consensus/utils/txscript/ctv_test.go
+++ domain/consensus/utils/txscript/ctv_test.go
@@ -8,6 +8,92 @@ import (
 	"github.com/Hoosat-Oy/HTND/domain/consensus/utils/subnetworks"
 )
 
+func newBaseCTVTx() *externalapi.DomainTransaction {
+	return &externalapi.DomainTransaction{
+		Version: 1,
+		Inputs: []*externalapi.DomainTransactionInput{{
+			PreviousOutpoint: externalapi.DomainOutpoint{
+				TransactionID: *externalapi.NewDomainTransactionIDFromByteArray(&[externalapi.DomainHashSize]byte{0x01}),
+				Index:         0,
+			},
+			SignatureScript: nil,
+			Sequence:        111,
+			SigOpCount:      0,
+		}, {
+			PreviousOutpoint: externalapi.DomainOutpoint{
+				TransactionID: *externalapi.NewDomainTransactionIDFromByteArray(&[externalapi.DomainHashSize]byte{0x02}),
+				Index:         1,
+			},
+			SignatureScript: nil,
+			Sequence:        222,
+			SigOpCount:      0,
+		}},
+		Outputs: []*externalapi.DomainTransactionOutput{{
+			Value: 7,
+			ScriptPublicKey: &externalapi.ScriptPublicKey{
+				Version: 0,
+				Script:  mustParseShortForm("1", 0),
+			},
+		}, {
+			Value: 9,
+			ScriptPublicKey: &externalapi.ScriptPublicKey{
+				Version: 0,
+				Script:  mustParseShortForm("1", 0),
+			},
+		}},
+		LockTime:     12345,
+		SubnetworkID: subnetworks.SubnetworkIDNative,
+		Gas:          0,
+		Payload:      nil,
+	}
+}
+
+func mustExecuteCTV(t *testing.T, tx *externalapi.DomainTransaction, txIdx int, templateHashBytes []byte) {
+	t.Helper()
+
+	script, err := NewScriptBuilder().
+		AddData(templateHashBytes).
+		AddOp(OpCheckTemplateVerify).
+		AddOp(OpTrue).
+		Script()
+	if err != nil {
+		t.Fatalf("script builder: %v", err)
+	}
+
+	vm, err := NewEngine(&externalapi.ScriptPublicKey{Script: script, Version: 2}, tx, txIdx, 0, nil, nil, &consensushashing.SighashReusedValues{})
+	if err != nil {
+		t.Fatalf("NewEngine: %v", err)
+	}
+	if err := vm.Execute(); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
+
+func mustFailCTV(t *testing.T, tx *externalapi.DomainTransaction, txIdx int, templateHashBytes []byte) {
+	t.Helper()
+
+	script, err := NewScriptBuilder().
+		AddData(templateHashBytes).
+		AddOp(OpCheckTemplateVerify).
+		AddOp(OpTrue).
+		Script()
+	if err != nil {
+		t.Fatalf("script builder: %v", err)
+	}
+
+	vm, err := NewEngine(&externalapi.ScriptPublicKey{Script: script, Version: 2}, tx, txIdx, 0, nil, nil, &consensushashing.SighashReusedValues{})
+	if err != nil {
+		t.Fatalf("NewEngine: %v", err)
+	}
+	err = vm.Execute()
+	if err == nil {
+		t.Fatalf("expected error")
+	}
+	if !IsErrorCode(err, ErrCheckTemplateVerify) {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
+
 func TestCheckTemplateVerify_BadHashLength(t *testing.T) {
 	t.Parallel()
 
@@ -115,6 +201,76 @@ func TestCheckTemplateVerify_TxIdxSensitivity(t *testing.T) {
 	if !IsErrorCode(err, ErrCheckTemplateVerify) {
 		t.Fatalf("unexpected error: %v", err)
 	}
+}
+
+func TestCheckTemplateVerify_FieldSensitivity(t *testing.T) {
+	t.Parallel()
+
+	baseTx := newBaseCTVTx()
+	baseHash := calculateTemplateHash(baseTx, 0).ByteSlice()
+
+	// Sanity: the exact tx should pass.
+	mustExecuteCTV(t, newBaseCTVTx(), 0, baseHash)
+
+	t.Run("locktime", func(t *testing.T) {
+		tx := newBaseCTVTx()
+		tx.LockTime++
+		mustFailCTV(t, tx, 0, baseHash)
+	})
+
+	t.Run("gas", func(t *testing.T) {
+		tx := newBaseCTVTx()
+		tx.Gas = 1
+		mustFailCTV(t, tx, 0, baseHash)
+	})
+
+	t.Run("subnetwork", func(t *testing.T) {
+		tx := newBaseCTVTx()
+		tx.SubnetworkID = subnetworks.SubnetworkIDData
+		mustFailCTV(t, tx, 0, baseHash)
+	})
+
+	t.Run("input prevout index", func(t *testing.T) {
+		tx := newBaseCTVTx()
+		tx.Inputs[0].PreviousOutpoint.Index++
+		mustFailCTV(t, tx, 0, baseHash)
+	})
+
+	t.Run("input sequence", func(t *testing.T) {
+		tx := newBaseCTVTx()
+		tx.Inputs[0].Sequence++
+		mustFailCTV(t, tx, 0, baseHash)
+	})
+
+	t.Run("input order", func(t *testing.T) {
+		tx := newBaseCTVTx()
+		tx.Inputs[0], tx.Inputs[1] = tx.Inputs[1], tx.Inputs[0]
+		mustFailCTV(t, tx, 0, baseHash)
+	})
+
+	t.Run("output value", func(t *testing.T) {
+		tx := newBaseCTVTx()
+		tx.Outputs[0].Value++
+		mustFailCTV(t, tx, 0, baseHash)
+	})
+
+	t.Run("output order", func(t *testing.T) {
+		tx := newBaseCTVTx()
+		tx.Outputs[0], tx.Outputs[1] = tx.Outputs[1], tx.Outputs[0]
+		mustFailCTV(t, tx, 0, baseHash)
+	})
+
+	t.Run("output count", func(t *testing.T) {
+		tx := newBaseCTVTx()
+		tx.Outputs = append(tx.Outputs, &externalapi.DomainTransactionOutput{
+			Value: 1,
+			ScriptPublicKey: &externalapi.ScriptPublicKey{
+				Version: 0,
+				Script:  mustParseShortForm("1", 0),
+			},
+		})
+		mustFailCTV(t, tx, 0, baseHash)
+	})
 }
 
 func TestCheckTemplateVerify_SubnetworkPayloadAffectsHash(t *testing.T) {
