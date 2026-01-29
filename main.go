996cedf3f8f6cdf8e065d38984e600e63094c9dd
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2026-01-29 11:14:56 +0200

    Debug information

--- main.go
+++ main.go
@@ -5,22 +5,26 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	_ "net/http/pprof"
 	"os"
 	"runtime"
+	"runtime/debug"
 
 	"github.com/Hoosat-Oy/HTND/app"
 )
 
 func main() {
+
 	runtime.GOMAXPROCS(runtime.NumCPU()) // Set the maximum number of CPUs that can be executing simultaneously
-	//debug.SetGCPercent(200)              // Run GoGC at 200% for less frequent garbage collection
-	// debug.SetMemoryLimit(8_000_000_000) // Set memory soft limit to 16GB
-	//runtime.SetBlockProfileRate(1)     // Set block profile rate to 1 to enable block profiling
-	//runtime.SetMutexProfileFraction(1) // Set mutex profile fraction to 1 to enable mutex profiling
-	// go func() {
-	// 	log.Println(http.ListenAndServe("localhost:6060", nil))
-	// }()
+	debug.SetGCPercent(200)              // Run GoGC at 200% for less frequent garbage collection
+	debug.SetMemoryLimit(8_000_000_000)  // Set memory soft limit to 16GB
+	runtime.SetBlockProfileRate(1)       // Set block profile rate to 1 to enable block profiling
+	runtime.SetMutexProfileFraction(1)   // Set mutex profile fraction to 1 to enable mutex profiling
+	go func() {
+		log.Println(http.ListenAndServe("127.0.0.1:6060", nil))
+	}()
 	if err := app.StartApp(); err != nil {
 		os.Exit(1)
 	}
